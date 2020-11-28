e2b809186e8afae46586fdaf22c0495c8664868d
ntestoc3 <ntestoc3@outlook.com>
2020-11-28 18:40:06 +0800

    修正table辅助函数

--- src/burp_clj/table_util.clj
+++ src/burp_clj/table_util.clj
@@ -1,7 +1,7 @@
 (ns burp-clj.table-util
+  "table辅助函数"
   (:require [seesaw.table :as table]
             [seesaw.core :as gui]))
-
 
 (defn- filter-table-by
   "返回[[row-index row-value]...]格式的序列"
@@ -9,25 +9,41 @@
   (->> (table/row-count tbl)
        (range)
        (table/value-at tbl)
-       (map-indexed (fn [idx v]
-                      [idx (f v)]))
-       (filter second)))
+       (map-indexed vector)
+       (filter (comp f second))))
 
 (defn insert-by!
-  [tbl f value]
-  (->>  (filter-table-by tbl f)
-        (mapcat #(vector %1 value))
-        (apply table/insert-at! tbl)))
+  "根据条件查找并插入新行
+  filter-pred 过滤函数，参数为table row数据
+  value 要插入的数据，格式同`seesaw.table/insert-at!`的value
+  after 是否在查找的行之后插入，默认为false"
+  ([tbl filter-pred value] (insert-by! tbl filter-pred value false))
+  ([tbl filter-pred value after]
+   (->>  (filter-table-by tbl filter-pred)
+         (mapcat (fn [[idx v]]
+                   [(if after
+                      (inc idx)
+                      idx)
+                    value]))
+         (apply table/insert-at! tbl))))
 
 (defn update-by!
-  [tbl f value]
-  (->> (filter-table-by tbl f)
-       (mapcat #(vector %1 value))
+  "根据条件查找并更新行
+  filter-pred 过滤函数，参数为table row数据
+  update-fn 更新函数，参数为table row数据
+  "
+  [tbl filter-pred update-fn]
+  (->> (filter-table-by tbl filter-pred)
+       (mapcat (fn [[idx v]]
+                 [idx (update-fn v)]))
        (apply table/update-at! tbl)))
 
 (defn remove-by!
-  [tbl f]
-  (->> (filter-table-by tbl f)
+  "根据条件查找并删除行
+  filter-pred 过滤函数，参数为table row数据
+  "
+  [tbl filter-pred]
+  (->> (filter-table-by tbl filter-pred)
        (map first)
        (apply table/remove-at! tbl)))
 
